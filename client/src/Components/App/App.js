9ca42a35a231477f0fe79f7ae9b764888862bb9c
omar <omar.csse@gmail.com>
2020-04-16 08:18:20 +1000

    adjust apollo to use cache and cookies

--- client/src/Components/App/App.js
+++ client/src/Components/App/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloProvider } from "react-apollo";
 import Nav from '../Nav/Nav';
 import Home from '../Home/Home';
 import Auth from '../Auth/Auth';
 import './App.scss';
 
+
 const client = new ApolloClient({
-	uri: 'http://localhost:4000/graphql',
-})
+    link: createHttpLink({
+        uri: "http://localhost:4000/graphql",
+        credentials: "include"
+    }),
+    cache: new InMemoryCache()
+});
 
 const App = () => {
 	return (
