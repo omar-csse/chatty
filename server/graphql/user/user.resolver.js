b6270d5bd9bc0ca51f04491b41abf0d14bc486cd
omar <omar.csse@gmail.com>
2020-04-19 09:02:27 +1000

    adjust schema, split them based on actions

--- server/graphql/user/user.resolver.js
+++ server/graphql/user/user.resolver.js
@@ -1,8 +1,4 @@
-const login = require('../../models/db/login.js');
-const logout = require('../../models/db/logout.js');
-const signup = require('../../models/db/signup.js');
 const getUser = require('../../models/db/getUser.js');
-const reset_password = require('../../models/db/reset_password.js');
 const { AuthenticationError } = require('apollo-server-express')
 
 
@@ -11,24 +7,16 @@ module.exports = resolvers = {
         user: async (_, {username}, {req, res}) => {
             if (!req.loggedIn) throw new AuthenticationError('Not Logged In');
 			return await getUser(username);
-        },
-        login: async (_, {identifier, password}, {req, res}) => {
-			return await login(identifier, password, res);
-        },
-        logout: (_, __, {req, res}) => {
-            return logout(res)
-        },
-        forget_password: (_, {email}, context) => {
-            return false
         }
     },
     Mutation: {
-        signup: async (_, {username, email, password}) => {
-			return await signup(username, email, password);
-        },
-        reset_password: async (_, {email, oldPass, newPass}, {req, res}) => {
+        follow_user: async (_, {username}, {req, res}) => {
             if (!req.loggedIn) throw new AuthenticationError('Not Authenticated');
-            return await reset_password(email, req.username, oldPass, newPass)
+			return true;
+        },
+        unfollow_user: async (_, {username}, {req, res}) => {
+            if (!req.loggedIn) throw new AuthenticationError('Not Authenticated');
+            return true;
         }
     }
 }
